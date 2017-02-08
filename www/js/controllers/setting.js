7364341ba7d480f80439d5d6c00d68da25b7898a
Eric Wang <skygragon@gmail.com>
2017-02-08 20:43:35 +0800

    Quick choose filter in setting page.
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- www/js/controllers/setting.js
+++ www/js/controllers/setting.js
@@ -1,23 +1,36 @@
 angular.module('Controllers')
 .controller('SettingController', function($scope, $cordovaFile, DB, Stat) {
+  $scope.IOing = false;
   $scope.algos = ['Random', 'Sequential'];
-  $scope.tags = Stat.tags;
   $scope.companies = ['Apple', 'Amazon', 'Facebook', 'Google', 'Microsoft'];
   $scope.counts = ['1', '2', '4', '8', '16', '24', '32'];
+  $scope.filters = [
+    {name: 'All', status: '1', tag: '', company: ''},
+    {name: 'UnResolved', status: '0', tag: '', company: ''}
+  ];
 
-  var dir;
-  try {
-    dir = cordova.file.externalDataDirectory || cordova.file.dataDirectory;
-  } catch (e) {
-    console.log(e.message);
-    dir = './'; // FIXME: hack web test where no cordova defined...
-  }
-  var name = 'c3.json';
-  $scope.fullpath = dir + name;
+  $scope.init = function() {
+    var filedir = './'; // FIXME: hack web test where no cordova defined...
+    try {
+      filedir = cordova.file.externalDataDirectory || cordova.file.dataDirectory;
+    } catch (e) {
+      console.log(e.message);
+    }
+    $scope.filedir = filedir;
+    $scope.filename = 'c3.json';
+    $scope.filepath = filedir + $scope.filename;
 
-  $scope.filter = Stat.filter;
-  $scope.updated = Stat.updated;
-  $scope.IOing = false;
+    $scope.filter = Stat.filter;
+    $scope.updated = Stat.updated;
+    $scope.tags = Stat.tags;
+    $scope.tags.forEach(function(tag) {
+      $scope.filters.push({name: tag, status: '1', tag: tag, company: ''});
+    });
+  };
+
+  $scope.setFilter = function(f) {
+    _.extendOwn($scope.filter, _.omit(f, 'name'));
+  };
 
   $scope.backup = function() {
     $scope.IOing = true;
@@ -28,7 +41,7 @@ angular.module('Controllers')
         } else {
           var msg = 'Backup ' + questions.length + ' questions: ';
           var data = JSON.stringify(questions);
-          $cordovaFile.writeFile(dir, name, data, true)
+          $cordovaFile.writeFile($scope.filedir, $scope.filename, data, true)
             .then(
               function(ok) {
                 alert(msg + 'ok');
@@ -49,7 +62,7 @@ angular.module('Controllers')
     }
     // TODO: add file selection dialog
     $scope.IOing = true;
-    $cordovaFile.readAsText(dir, name)
+    $cordovaFile.readAsText($scope.filedir, $scope.filename)
       .then(
         function(text) {
           var questions = JSON.parse(text);
@@ -70,4 +83,6 @@ angular.module('Controllers')
         }
       );
   };
+
+  $scope.init();
 });
